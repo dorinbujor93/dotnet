ab71809d4f4738955018d83388f3b9ad2566a839
Dorin Bujor <dorin.bujor@webbeds.com>
2020-03-06 15:55:14 +0200

    Ex 2

--- week_1/class/W1_Class/W1_Class/Program.cs
+++ week_1/class/W1_Class/W1_Class/Program.cs
@@ -6,10 +6,16 @@ namespace W1_Class
     {
         static void Main(string[] args)
         {
+            //Exercise1();
+            Exercise2();
+        }
+
+        static void Exercise1()
+        {
             int len = 3;
             char[] chars = new char[len];
 
-            for(int i =0; i<len; i++)
+            for (int i = 0; i < len; i++)
             {
                 Console.WriteLine("\nInsert a letter:  ");
 
@@ -20,6 +26,44 @@ namespace W1_Class
             for (int i = 0; i < len; i++)
             {
                 Console.WriteLine(chars[i]);
+            }
+
+        }
+        static void Exercise2()
+        {
+            char operation;
+            int firstNr;
+            int secondNr;
+            int result;
+            Console.WriteLine("Please insert first number: \n");
+            firstNr = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please choose an operation from (+,-,*,/)\n");
+            operation = Console.ReadKey().KeyChar;
+
+            Console.WriteLine("Please insert second number: \n");
+            secondNr = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (operation)
+            {
+                case '+':
+                    result = firstNr + secondNr;
+                    Console.WriteLine($"\n You result is: {result}" );
+                    break;
+                case '-':
+                    result = firstNr - secondNr;
+                    Console.WriteLine($"\n You result is: {result}");
+                    break;
+                case '*':
+                    result = firstNr * secondNr;
+                    Console.WriteLine($"\n You result is: {result}");
+                    break;
+                case '/':
+                    result = firstNr / secondNr;
+                    Console.WriteLine($"\n You result is: {result}");
+                    break;
+        
             }
         }
     }
