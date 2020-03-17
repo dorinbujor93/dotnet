5d359b533f8c54ef0493f2790dd88d45176475ae
Dorin <dor-in@mail.ru>
2020-03-17 16:48:52 +0200

    Week_2 Ex2 BoilerPlate

---
+++ week_2/homework/W2_Homework/HotelApp/CarStore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CarStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Alex intended to buy a Ford Focus 2015 model.
+            //He walked to the FordStore in Bucuresti and agreed to buy one for *15000Euro.
+            //They informed him it will take 4 weeks for delivery.
+            //He then decided to visit another store SkodaStore and agreed to buy one * for 14000Euro and 3 weeks delivery.
+            //He then canceled his original order from the FordStore.
+            //After 3 weeks, he received the model.
+
+
+
+        }
+    }
+}
