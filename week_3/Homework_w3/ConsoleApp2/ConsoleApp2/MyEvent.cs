f96f651244009e619dbfa604e56d3a0619823ad1
Dorin <dor-in@mail.ru>
2020-03-20 16:07:03 +0200

    ex 2

---
+++ week_3/Homework_w3/ConsoleApp2/ConsoleApp2/MyEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public delegate void OnMyEventHandler(object sender, EventArgs e);
+
+   public class MyEvent
+    {
+        public event OnMyEventHandler MyEv;
+
+        public void OnMyEvent()
+        {
+            if (MyEv != null)
+            {
+                MyEv(this, EventArgs.Empty);
+            }
+        }
+    }
+
+}
---
+++ week_3/Homework_w3/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var evernt = new MyEvent();
+            evernt.MyEv += new OnMyEventHandler(Callback);
+
+
+            while (true)
+            {
+
+                evernt.OnMyEvent();
+                Thread.Sleep(2000);
+            }
+        }
+        public static void Callback(object sender, EventArgs e)
+        {
+            Console.WriteLine("Callback FCT");
+        }
+    }
+}
