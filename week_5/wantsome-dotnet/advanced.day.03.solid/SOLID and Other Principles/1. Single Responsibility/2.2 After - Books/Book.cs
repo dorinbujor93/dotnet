4a36087154b9b8c19b2f3779ce10fb7196566a80
Dorin <dor-in@mail.ru>
2020-06-09 09:54:14 +0300

    SOLID repo

---
+++ week_5/wantsome-dotnet/advanced.day.03.solid/SOLID and Other Principles/1. Single Responsibility/2.2 After - Books/Book.cs
@@ -0,0 +1,11 @@
+namespace SingleResponsibilityBooksAfter
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Location { get; set; }
+    }
+}
---
+++ week_5/wantsome-dotnet/advanced.day.03.solid/SOLID and Other Principles/1. Single Responsibility/2.2 After - Books/PageManager.cs
@@ -0,0 +1,12 @@
+namespace SingleResponsibilityBooksAfter
+{
+    using Book;
+
+    public class PageManager
+    {
+        public string TurnPage(Book book, int page)
+        {
+            return "Current page";
+        }
+    }
+}
